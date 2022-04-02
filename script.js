bc3136e42b0f74e7047918dfa8f99cd30643290f
darkgx207 <guilhermerocha207@gmail.com>
2022-04-02 18:47:18 -0300

    Projeto v1.0

---
+++ script.js
@@ -0,0 +1,33 @@
+const entryf = document.querySelector('.entry');
+const submitb = document.querySelector('.submitbtn');
+let nitem = document.querySelector('.ntask').childElementCount;
+
+function memory(x){
+    entryf.value = '';
+    const ntask = document.querySelector('.ntask');
+    const newe = document.createElement('li');
+    newe.innerText = `(${nitem}) ${x}` ;
+    newe.classList.add(`li${nitem}`)
+    ntask.appendChild(newe)
+}
+function adding(){
+    const limite = document.querySelector('.ntask').childElementCount;
+    if(entryf.value !== ''){
+        if(limite > 20) return;
+        memory(entryf.value);
+        nitem++;
+    }
+}
+    
+function addbtn() {
+
+    submitb.addEventListener('click',adding);
+    entryf.addEventListener('keypress',(e)=>{
+        if(e.key === 'Enter') adding();
+    }
+    
+    );
+}
+
+addbtn();
+
